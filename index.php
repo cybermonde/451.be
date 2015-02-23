197e0cbbab79505f9dec06e0d1097bd06443a198
451.be <legal@451.be>
2015-02-23 19:03:19 +0100

    changement lay-out

--- index.php
+++ index.php
@@ -38,8 +38,8 @@
           </div>
 
           <div class="inner cover">
-            <h1 class="cover-heading">451.be</h1>
-            <p class="lead">HTTP/1.1 451 Unavailable For Legal Reasons</p>
+            <h1 class="cover-heading">HTTP/1.1 451</h1>
+            <p class="lead">Unavailable For Legal Reasons</p>
             <p class="lead">
               <a href="http://tools.ietf.org/id/draft-tbray-http-legally-restricted-status-00.html" class="btn btn-lg btn-default" target="_blank">Learn more</a>
             </p>
